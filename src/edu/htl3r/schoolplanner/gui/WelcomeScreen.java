c0897dfe545041c01a6653b4766a631c2fd7641f
basti <sebastian@schoolplanner.at>
2011-06-16 22:50:06 +0200

    * welcome screen add * android manifest clenup

---
+++ src/edu/htl3r/schoolplanner/gui/WelcomeScreen.java
@@ -0,0 +1,13 @@
+package edu.htl3r.schoolplanner.gui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WelcomeScreen extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
