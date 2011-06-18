89941192ac4fd3cdc8ee61e447a36d8f2fb945e9
Mathias Kub <git@makubi.at>
2011-06-18 20:50:18 +0200

    Hinzufuegen von Eintraeg zur Liste in Methode ausgelagert.

--- src/edu/htl3r/schoolplanner/gui/WelcomeScreen.java
+++ src/edu/htl3r/schoolplanner/gui/WelcomeScreen.java
@@ -1,8 +1,6 @@
 package edu.htl3r.schoolplanner.gui;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import android.app.Activity;
@@ -12,39 +10,31 @@ import edu.htl3r.schoolplanner.R;
 
 public class WelcomeScreen extends Activity{
 	
+	private TableLayout tl;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.welcome);
 		//setContentView(R.layout.welcome2);
 		
-		List<Map<String,String>> data = new ArrayList<Map<String,String>>();
-		
-		Map<String, String> data1 = new HashMap<String, String>();
-		data1.put("name", "HTL Rennweg");
-		data1.put("url", "urania.webuntis.com");
-		data1.put("school", "htl3r");
-		data1.put("user", "htl3r");
-		
-		Map<String, String> data2 = new HashMap<String, String>();
-		data2.put("name", "WebUntis Testschule");
-		data2.put("url", "webuntis.grupet.at:8080");
-		data2.put("school", "demo");
-		data2.put("user", "user");
+		tl = (TableLayout) findViewById(R.id.tableLayout1);
 
-		data.add(data1);
-		data.add(data2);
+		addEntry("HTL Rennweg", "urania.webuntis.com", "htl3r", "htl3r");
+		addEntry("WebUntis Testschule", "webuntis.grupet.at:8080", "demo", "user");
+	}
+	
+	private void addEntry(String name, String url, String school, String user) {
+		Map<String, String> dataEntry = new HashMap<String, String>();
+		dataEntry.put("name", name);
+		dataEntry.put("url", "webuntis.grupet.at:8080");
+		dataEntry.put("school", "demo");
+		dataEntry.put("user", "user");
 		
-		TableLayout tl = (TableLayout) findViewById(R.id.tableLayout1);
-
-		for(Map<String, String> dataEntry : data) {
-			DataSetEntry entry = new DataSetEntry(this);
-			entry.setTableLayout(tl);
-			entry.setDataEntry(dataEntry);
-			entry.draw();
-		}
-		
-		
+		DataSetEntry entry = new DataSetEntry(this);
+		entry.setTableLayout(tl);
+		entry.setDataEntry(dataEntry);
+		entry.draw();
 	}
 }
 
