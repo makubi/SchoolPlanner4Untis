99f113d51e8d5926193cf9a78b8ae01f7c02a933
Mathias Kub <git@makubi.at>
2011-08-30 13:37:33 +0200

    Erste Schritte zum Speichern der Stunden in der Datenbank.

--- src/edu/htl3r/schoolplanner/backend/database/constants/DatabaseCreateConstants.java
+++ src/edu/htl3r/schoolplanner/backend/database/constants/DatabaseCreateConstants.java
@@ -18,34 +18,43 @@ package edu.htl3r.schoolplanner.backend.database.constants;
 
 public interface DatabaseCreateConstants {
 
-	public static final String TABLE_VIEW_TYPE_COLUMN_DEFINITIONS = DatabaseViewTypeConstants.ID
-			+ " INTEGER, "
-			+ DatabaseViewTypeConstants.NAME
-			+ " TEXT, "
-			+ DatabaseViewTypeConstants.LONG_NAME
-			+ " TEXT, "
-			+ DatabaseViewTypeConstants.FORE_COLOR
-			+ " TEXT, "
+	public static final String TABLE_VIEW_TYPE_COLUMN_DEFINITIONS =
+			  DatabaseViewTypeConstants.ID + " INTEGER, "
+			+ DatabaseViewTypeConstants.NAME + " TEXT, "
+			+ DatabaseViewTypeConstants.LONG_NAME + " TEXT, "
+			+ DatabaseViewTypeConstants.FORE_COLOR + " TEXT, "
 			+ DatabaseViewTypeConstants.BACK_COLOR + " TEXT";
 
-	public static final String TABLE_SCHOOL_HOLIDAY_COLUMN_DEFINITIONS = DatabaseSchoolHolidayConstants.ID
-			+ " INTEGER, "
-			+ DatabaseSchoolHolidayConstants.NAME
-			+ " TEXT, "
-			+ DatabaseSchoolHolidayConstants.LONG_NAME
-			+ " TEXT, "
-			+ DatabaseSchoolHolidayConstants.START_DATE
-			+ " INTEGER, "
+	public static final String TABLE_SCHOOL_HOLIDAY_COLUMN_DEFINITIONS =
+			  DatabaseSchoolHolidayConstants.ID + " INTEGER, "
+			+ DatabaseSchoolHolidayConstants.NAME + " TEXT, "
+			+ DatabaseSchoolHolidayConstants.LONG_NAME + " TEXT, "
+			+ DatabaseSchoolHolidayConstants.START_DATE + " INTEGER, "
 			+ DatabaseSchoolHolidayConstants.END_DATE + " INTEGER";
 
-	public static final String TABLE_TIMEGRID_COLUMN_DEFINITIONS = DatabaseTimegridConstants.DAY
-			+ " INTEGER, "
-			+ DatabaseTimegridConstants.START_TIME
-			+ " INTEGER, " + DatabaseTimegridConstants.END_TIME + " INTEGER";
+	public static final String TABLE_TIMEGRID_COLUMN_DEFINITIONS =
+			  DatabaseTimegridConstants.DAY + " INTEGER, "
+			+ DatabaseTimegridConstants.START_TIME + " INTEGER, "
+			+ DatabaseTimegridConstants.END_TIME + " INTEGER";
 
-	public static final String TABLE_STATUS_DATA_COLUMN_DEFINITIONS = DatabaseStatusDataConstants.CODE
-			+ " TEXT, "
-			+ DatabaseStatusDataConstants.FORE_COLOR
-			+ " INTEGER, "
+	public static final String TABLE_STATUS_DATA_COLUMN_DEFINITIONS =
+			  DatabaseStatusDataConstants.CODE + " TEXT, "
+			+ DatabaseStatusDataConstants.FORE_COLOR + " INTEGER, "
 			+ DatabaseStatusDataConstants.BACK_COLOR + " INTEGER";
+	
+	public static final String TABLE_LESSONS_COLUMN_DEFINITIONS =
+			  DatabaseLessonConstants.ID+ " INTEGER, "
+			+ DatabaseLessonConstants.DATE + " INTEGER, "
+			+ DatabaseLessonConstants.START_TIME + " INTEGER, "
+			+ DatabaseLessonConstants.END_TIME + " INTEGER, "
+			// TODO: Als Liste von IDs speichern?
+			+ DatabaseLessonConstants.SCHOOL_CLASS_LIST + " TEXT, "
+			// TODO: Als Liste von IDs speichern?
+			+ DatabaseLessonConstants.SCHOOL_TEACHER_LIST + " TEXT, "
+			// TODO: Als Liste von IDs speichern?
+			+ DatabaseLessonConstants.SCHOOL_ROOM_LIST + " TEXT, "
+			// TODO: Als Liste von IDs speichern?
+			+ DatabaseLessonConstants.SCHOOL_SUBJECT_LIST + " TEXT, "
+			+ DatabaseLessonConstants.LESSON_TYPE + " TEXT, "
+			+ DatabaseLessonConstants.LESSON_CODE + " TEXT";
 }
---
+++ src/edu/htl3r/schoolplanner/backend/database/constants/DatabaseLessonConstants.java
@@ -0,0 +1,46 @@
+/* SchoolPlanner4Untis - Android app to manage your Untis timetable
+    Copyright (C) 2011  Mathias Kub <mail@makubi.at>
+			Sebastian Chlan <sebastian@schoolplanner.at>
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package edu.htl3r.schoolplanner.backend.database.constants;
+
+
+public interface DatabaseLessonConstants {
+	
+	public static final String TABLE_LESSONS_NAME = "lessons";
+	
+	public static final String ID = "id";
+	
+	public static final String DATE = "date";
+	public static final String START_TIME = "startTime";
+	public static final String END_TIME = "endTime";
+	
+	public static final String SCHOOL_CLASS_LIST = "schoolClassList";
+	public static final String SCHOOL_TEACHER_LIST = "schoolTeacherList";
+	public static final String SCHOOL_SUBJECT_LIST = "schoolSubjectList";
+	public static final String SCHOOL_ROOM_LIST = "schoolRoomList";
+	
+	public static final String LESSON_TYPE = "lessonType";
+	public static final String LESSON_CODE = "lessonCode";
+	
+	public static final String LESSON_TYPE_BREAK_SUPERVISION = "lessonTypeBreakSupervision";
+	public static final String LESSON_TYPE_EXAMINATION = "lessonTypeExamination";
+	public static final String LESSON_TYPE_OFFICE_HOUR = "lessonTypeOfficeHour";
+	public static final String LESSON_TYPE_STANDBY = "lessonTypeStandby";
+	
+	public static final String LESSON_CODE_CANCELLED = "lessonCodeCancelled";
+	public static final String LESSON_CODE_IRREGULAR = "lessonCodeIrregular";
+	public static final String LESSON_CODE_SUBSTITUTE = "lessonCodeSubstitute";
+}
