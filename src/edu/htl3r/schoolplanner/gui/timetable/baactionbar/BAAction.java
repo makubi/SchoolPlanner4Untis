219f30122355bcb0265ca0eb2bc55bb0ac0173ab
Sebastian <sebastian.chlan@gmail.com>
2011-11-27 17:41:23 +0100

    Onclick animation fuer actionbar actions

--- src/edu/htl3r/schoolplanner/gui/timetable/baactionbar/BAAction.java
+++ src/edu/htl3r/schoolplanner/gui/timetable/baactionbar/BAAction.java
@@ -1,26 +1,38 @@
 package edu.htl3r.schoolplanner.gui.timetable.baactionbar;
 
+import edu.htl3r.schoolplanner.R;
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
-public class BAAction extends RelativeLayout{
+public class BAAction extends RelativeLayout implements OnTouchListener{
 	
 	private ImageView icon;
 	
 	public BAAction(Context context) {
 		super(context);
+		init();
 	}
 	
 	public BAAction(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		init();
 	}
 	
 	public BAAction(Context context, AttributeSet attrs, int defStyle){
 		super(context, attrs, defStyle);
+		init();
+	}
+	
+	private void init(){
+		setOnTouchListener(this);
 	}
 	
 	public void setIcon(Drawable drawable){
@@ -30,6 +42,17 @@ public class BAAction extends RelativeLayout{
 		layout.addRule(CENTER_VERTICAL);
 		layout.addRule(CENTER_HORIZONTAL);
 		addView(icon, layout);
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		if(event.getAction() == MotionEvent.ACTION_DOWN){
+			setBackgroundColor(getResources().getColor(R.color.month_overlay_today));
+		}
+		if(event.getAction() == MotionEvent.ACTION_UP){
+			setBackgroundColor(Color.parseColor("#00000000"));
+		}
+		return false;
 	}
 
 	
