3879b4f275df8f2da482520e563592b7a42e6fd4
Mathias Kub <git@makubi.at>
2012-04-01 13:12:36 +0200

    StartupWizardLoginInformationCheck ist scrollbar.

--- src/edu/htl3r/schoolplanner/gui/startup_wizard/StartupWizardLoginInformationCheck.java
+++ src/edu/htl3r/schoolplanner/gui/startup_wizard/StartupWizardLoginInformationCheck.java
@@ -2,8 +2,8 @@ package edu.htl3r.schoolplanner.gui.startup_wizard;
 
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.os.Bundle;
 import android.os.AsyncTask.Status;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
